00904df88fdde7ce3baaeb3b1f6711bdc892128c
asimonia <alex.simonian@gmail.com>
2016-10-31 15:17:30 -0500

    Produts display on DOM

--- DOMHandler.js
+++ DOMHandler.js
@@ -1,9 +1,22 @@
-function showCategories() {
+function showCategories(categories) {
 
 }
 
-function showProducts() {
-  
+function showProducts(products) {
+	var list = document.getElementById("products");
+	var outputString = "";
+
+	for (var i = 0; i < products.length; i++) {
+		var currentProduct = products[i];
+		outputString += `<tr>
+		<td>${currentProduct.name}</td>
+		<td>${currentProduct.category_id}</td>
+		<td>${currentProduct.price}</td>
+    </tr>`
+      	console.log(outputString);
+	}
+
+	list.innerHTML = outputString;
 }
 
 SeasonalSales.loadCategories(showCategories);
